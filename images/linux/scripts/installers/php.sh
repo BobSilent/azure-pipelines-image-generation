67786970b0d93c4f4b149281e9ab069db2fa1880
BobSilent <Andre_1@gmx.net>
2019-04-08 11:42:07 +0200

    PHP Composer Install use wget
    Instead of using php script, wget considers a configured Proxy for accessing the internet

--- images/linux/scripts/installers/php.sh
+++ images/linux/scripts/installers/php.sh
@@ -238,7 +238,7 @@ apt-get remove --purge -yq php7.2-dev
 apt-fast install -y --no-install-recommends snmp
 
 # Install composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+wget -O composer-setup.php https://getcomposer.org/installer
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer for Composer verified is verified.'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 sudo mv composer.phar /usr/bin/composer
